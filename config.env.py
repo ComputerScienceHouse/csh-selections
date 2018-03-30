dafb61dd56069adb1a3268d9a2816da94b70a93e
SkylerJaneClark <puffballinthedark@gmail.com>
2018-03-30 15:42:36 -0400

    adds a base for all that stuff

--- config.env.py
+++ config.env.py
@@ -7,7 +7,7 @@ from os import environ as env
 DEBUG = True
 IP = os.environ.get('IP', 'localhost')
 PORT = os.environ.get('PORT', 8080)
-SERVER_NAME = os.environ.get('SERVER_NAME', 'selections2.csh.rit.edu')
+SERVER_NAME = os.environ.get('SERVER_NAME', 'selections-dev.csh.rit.edu')
 
 # DB Info
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///{}'.format(os.path.join(os.getcwd(), "data.db")))
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", default=''.join(random.SystemRandom().choic
 # OpenID Connect SSO config
 OIDC_ISSUER = os.environ.get('OIDC_ISSUER', 'https://sso.csh.rit.edu/auth/realms/csh')
 OIDC_CLIENT_CONFIG = {
-    'client_id': os.environ.get('OIDC_CLIENT_ID', 'profiles'),
+    'client_id': os.environ.get('OIDC_CLIENT_ID', 'selections'),
     'client_secret': os.environ.get('OIDC_CLIENT_SECRET', ''),
     'post_logout_redirect_uris': [os.environ.get('OIDC_LOGOUT_REDIRECT_URI', 'https://profiles.csh.rit.edu/logout')]
 }
--- selections/__init__.py
+++ selections/__init__.py
@@ -1,16 +1,32 @@
 from flask import Flask, render_template, flash
 import csh_ldap
 from flask_sqlalchemy import SQLAlchemy
+from flask_pyoidc.flask_pyoidc import OIDCAuthentication
 import os
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config.from_pyfile(os.path.join(os.getcwd(), "config.env.py"))
+
+if os.path.exists(os.path.join(os.getcwd(), "config.py")):
+    app.config.from_pyfile(os.path.join(os.getcwd(), "config.py"))
+else:
+    app.config.from_pyfile(os.path.join(os.getcwd(), "config.env.py"))
+
+auth = OIDCAuthentication(app, issuer=app.config["OIDC_ISSUER"],
+                                  client_registration_info=app.config["OIDC_CLIENT_CONFIG"])
+
+_ldap = csh_ldap.CSHLDAP(app.config['LDAP_BIND_DN'], app.config['LDAP_BIND_PASS'])
+
+
+
+from selections.utils import before_request, get_member_info, process_image
 
 app.secret_key = "listen, it's real secret"
 
 @app.route("/")
+@auth.oidc_auth
+@before_request
 def main():
     return render_template('index.html')
 
